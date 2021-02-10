cfb3771ed2c1f97b744296b8f64cf2cfc62a1113
kannoRyo <75112083+kannoRyo@users.noreply.github.com>
2021-02-10 09:36:11 +0900

    Delete src/firebase directory

--- src/firebase/config.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyC-aSZLiOXU3hwO7CeJAJ9aX7pcaeB1lmQ",
-    authDomain: "meeting-management-a2285.firebaseapp.com",
-    projectId: "meeting-management-a2285",
-    storageBucket: "meeting-management-a2285.appspot.com",
-    messagingSenderId: "462548768420",
-    appId: "1:462548768420:web:9a2035b9717d8995d4b831",
-    measurementId: "G-011CXHE6QP"
-  };
